3f3ca5df0eaedd0ea2311be965a1a7c4362fc5b0
Adam Skoufis <adam.skoufis@gmail.com>
2022-06-01 22:58:55 +1000

    Add some missing babel deps, add `jest-watch-typeahead`, make package private so I don't accidentally publish it to npm (again)

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   transform: {
     "^.+\\.(t|j)sx?$": ["@swc/jest"],
   },
   testEnvironment: "node",
   testPathIgnorePatterns: ["src/test", "out/"],
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
 };
